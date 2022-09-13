f3bbc2362c7b81efb16702e50b494509e6c7eada
aliakseikln <aliakseikln@gmail.com>
2022-09-13 22:13:11 +0300

    Added fun html converter and refactor DetailsScreen.kt

--- app/src/main/java/com/example/movies/utils/Constants.kt
+++ app/src/main/java/com/example/movies/utils/Constants.kt
@@ -1,6 +1,10 @@
 package com.example.movies.utils
 
-import com.example.movies.MainActivity
+import android.widget.TextView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
 
 class Constants {
 
@@ -9,4 +13,13 @@ class Constants {
         const val MAIN_SCREEN = "main_screen"
         const val DETAILS_SCREEN = "details_screen"
     }
+}
+
+@Composable
+fun HtmlText(html: String, modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context -> TextView(context) },
+        update = { it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT) }
+    )
 }
