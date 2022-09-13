dfb5590cc41db55e9a9974bd16ce7683161125d5
aliakseikln <aliakseikln@gmail.com>
2022-09-13 20:35:18 +0300

    Added ApiRepository.kt, ApiService.kt and Models.

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.movies.navigation.SetUpNavHost
 import com.example.movies.ui.theme.MoviesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
